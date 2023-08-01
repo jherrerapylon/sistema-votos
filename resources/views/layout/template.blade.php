dd1832c32be18ff67d1b15ba10ccf5422a1c00bf
jherrerapylon <123355812+jherrerapylon@users.noreply.github.com>
2023-08-01 12:19:58 +0200

    menu

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -8,9 +8,25 @@
     <title>@yield('title')</title>
 </head>
 <body>
-    
-    @yield('contenido')
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse px-5" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ url('mesas') }}">Mesas electorales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('partidos') }}">Partidos políticos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('participantes') }}">Participantes</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 
+    @yield('contenido')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
